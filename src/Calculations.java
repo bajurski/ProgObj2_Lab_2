04d7bb4d1fdc1dcd3de423a941393812f64e8405
bajurski <bajurski@hotmail.com>
2022-10-29 01:34:41 +0200

    Initial commit

---
+++ src/Calculations.java
@@ -0,0 +1,29 @@
+public class Calculations {
+    private final short[][] table = new short[11][11];
+
+    void calcTable() {
+
+        for (short i = 0; i < 11; i++) {
+            for (short j = 0; j < 11; j++) {
+                short val = (short) (i * j);
+                if (i == 0) val = j;
+                if (j == 0) val = i;
+                table[i][j] = val;
+
+            }
+        }
+        diplayTableInConsol(table);
+    }
+
+    void diplayTableInConsol(short[][] table) {
+        System.out.println(" _______________________________________________________");
+        for (short i = 0; i < 11; i++) {
+            for (short j = 0; j < 11; j++) {
+                if(i==0 && j==0)System.out.print(" |   ");
+                else if (j == 10) System.out.format("%4d%2s \n", table[i][j], "|");
+                else System.out.format("%2s%3d", "|", table[i][j]);
+            }
+            System.out.println(" -------------------------------------------------------");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculations calc = new Calculations();
+            calc.calcTable();
+        //System.out.println("Hello world!");
+    }
+}
