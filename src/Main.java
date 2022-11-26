6c0b7aad1e99af9ea833ef3240a5a2f9a42a470b
bajurski <bajurski@hotmail.com>
2022-11-26 11:29:55 +0100

    Project finished.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,5 @@ public class Main {
     public static void main(String[] args) {
         Calculations calc = new Calculations();
             calc.calcTable();
-        //System.out.println("Hello world!");
     }
 }
